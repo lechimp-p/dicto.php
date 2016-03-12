f18a23bb26de274e797bf160a153202687a14f1e
Richard Klees <richard.klees@rwth-aachen.de>
2016-03-12 13:48:59 +0100

    tests on variables with name.

--- tests/ArtifactSelectionTest.php
+++ tests/ArtifactSelectionTest.php
@@ -54,56 +54,176 @@ class BuildinMock implements Ver\BuildinArtifact {
 class ArtifactSelectionTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
         $this->class_one = new ClassMock("ClassOne");
+        $this->class_two = new ClassMock("ClassTwo");
         $this->function_one = new FunctionMock("function_one");
+        $this->function_two = new FunctionMock("function_two");
         $this->global_one = new GlobalMock("global_one");
+        $this->global_two = new GlobalMock("global_two");
         $this->file_one = new FileMock("file_one");
+        $this->file_two = new FileMock("file_two");
         $this->buildin_one = new BuildinMock("buildin_one");
+        $this->buildin_two = new BuildinMock("buildin_two");
 
         $this->selector = new Ver\Implementation\Selector;
     }
 
     public function test_match_every_class() {
         $var = Dicto::_every()->_class();
+
         $this->assertTrue($this->selector->matches($var, $this->class_one));
         $this->assertFalse($this->selector->matches($var, $this->function_one));
         $this->assertFalse($this->selector->matches($var, $this->global_one));
         $this->assertFalse($this->selector->matches($var, $this->file_one));
         $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertTrue($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
     }
 
     public function test_match_every_function() {
         $var = Dicto::_every()->_function();
+
         $this->assertFalse($this->selector->matches($var, $this->class_one));
         $this->assertTrue($this->selector->matches($var, $this->function_one));
         $this->assertFalse($this->selector->matches($var, $this->global_one));
         $this->assertFalse($this->selector->matches($var, $this->file_one));
         $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertTrue($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
     }
 
     public function test_match_every_global() {
         $var = Dicto::_every()->_global();
+
         $this->assertFalse($this->selector->matches($var, $this->class_one));
         $this->assertFalse($this->selector->matches($var, $this->function_one));
         $this->assertTrue($this->selector->matches($var, $this->global_one));
         $this->assertFalse($this->selector->matches($var, $this->file_one));
         $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertTrue($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
     }
 
     public function test_match_every_file() {
         $var = Dicto::_every()->_file();
+
         $this->assertFalse($this->selector->matches($var, $this->class_one));
         $this->assertFalse($this->selector->matches($var, $this->function_one));
         $this->assertFalse($this->selector->matches($var, $this->global_one));
         $this->assertTrue($this->selector->matches($var, $this->file_one));
         $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertTrue($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
     }
 
     public function test_match_every_buildin() {
         $var = Dicto::_every()->_buildin();
+
         $this->assertFalse($this->selector->matches($var, $this->class_one));
         $this->assertFalse($this->selector->matches($var, $this->function_one));
         $this->assertFalse($this->selector->matches($var, $this->global_one));
         $this->assertFalse($this->selector->matches($var, $this->file_one));
         $this->assertTrue($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertTrue($this->selector->matches($var, $this->buildin_two));
+    }
+
+    public function test_match_every_class_with_name() {
+        $var = Dicto::_every()->_class()->_with()->_name(".*One");
+
+        $this->assertTrue($this->selector->matches($var, $this->class_one));
+        $this->assertFalse($this->selector->matches($var, $this->function_one));
+        $this->assertFalse($this->selector->matches($var, $this->global_one));
+        $this->assertFalse($this->selector->matches($var, $this->file_one));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
+    }
+
+    public function test_match_every_function_with_name() {
+        $var = Dicto::_every()->_function()->_with()->_name(".*one");
+
+        $this->assertFalse($this->selector->matches($var, $this->class_one));
+        $this->assertTrue($this->selector->matches($var, $this->function_one));
+        $this->assertFalse($this->selector->matches($var, $this->global_one));
+        $this->assertFalse($this->selector->matches($var, $this->file_one));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
+    }
+
+    public function test_match_every_global_with_name() {
+        $var = Dicto::_every()->_global()->_with()->_name(".*one");
+
+        $this->assertFalse($this->selector->matches($var, $this->class_one));
+        $this->assertFalse($this->selector->matches($var, $this->function_one));
+        $this->assertTrue($this->selector->matches($var, $this->global_one));
+        $this->assertFalse($this->selector->matches($var, $this->file_one));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
+    }
+
+    public function test_match_every_file_with_name() {
+        $var = Dicto::_every()->_file()->_with()->_name(".*one");
+
+        $this->assertFalse($this->selector->matches($var, $this->class_one));
+        $this->assertFalse($this->selector->matches($var, $this->function_one));
+        $this->assertFalse($this->selector->matches($var, $this->global_one));
+        $this->assertTrue($this->selector->matches($var, $this->file_one));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertTrue($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
+    }
+
+    public function test_match_every_buildin_with_name() {
+        $var = Dicto::_every()->_buildin()->_with()->_name(".*one");
+
+        $this->assertFalse($this->selector->matches($var, $this->class_one));
+        $this->assertFalse($this->selector->matches($var, $this->function_one));
+        $this->assertFalse($this->selector->matches($var, $this->global_one));
+        $this->assertFalse($this->selector->matches($var, $this->file_one));
+        $this->assertTrue($this->selector->matches($var, $this->buildin_one));
+
+        $this->assertFalse($this->selector->matches($var, $this->class_two));
+        $this->assertFalse($this->selector->matches($var, $this->function_two));
+        $this->assertFalse($this->selector->matches($var, $this->global_two));
+        $this->assertFalse($this->selector->matches($var, $this->file_two));
+        $this->assertFalse($this->selector->matches($var, $this->buildin_two));
     }
 }
