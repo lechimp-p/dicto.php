8af1e6ca580a50d8d54a3652294b22106cb17017
Richard Klees <richard.klees@rwth-aachen.de>
2016-03-17 21:46:37 +0100

    naive implementation of ruleset.

--- src/Definition/Ruleset.php
+++ src/Definition/Ruleset.php
@@ -10,12 +10,39 @@
 
 namespace Lechimp\Dicto\Definition;
 
+/**
+ * A set of rules and variable definitions.
+ */
 class Ruleset {
-    public function variables() {
-        return array();
+    /**
+     * @var Variables\Variable[]
+     */
+    private $variables;
+
+    /**
+     * @var Rule[]  $rules
+     */
+    private $rules;
+
+    public function __construct(array $variables, array $rules) {
+        $this->variables = array_map(function (Variables\Variable $v) 
+                                        { return $v; }, $variables);
+
+        $this->rules = array_map(function (Rule $r) 
+                                        { return $r; }, $rules);
     }
 
+    /**
+     * @return  Variables\Variable[]
+     */
+    public function variables() {
+        return $this->variables;
+    }
+
+    /**
+     * @return  Rule[]
+     */
     public function rules() {
-        return array();
+        return $this->rules;
     }
 }
