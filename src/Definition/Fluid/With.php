b4bd975a02cde7490e85b201be9aedc58deb27a4
Richard Klees <richard.klees@rwth-aachen.de>
2016-03-19 21:42:35 +0100

    implemented With

--- src/Definition/Fluid/With.php
+++ src/Definition/Fluid/With.php
@@ -9,8 +9,23 @@
  */
 
 namespace Lechimp\Dicto\Definition\Fluid;
+use Lechimp\Dicto\Definition\Variables as Vars;
 
-class With {
-    public function name() {
+/**
+ * Provides fluid interface for with().
+ */
+class With extends Base {
+    /**
+     * Say to mean only artifacts whose name matches the provided regexp.
+     *
+     * @param   string  $regex
+     * @return  ExistingVar
+     */
+    public function name($regexp) {
+        $subject = $this->rt->get_current_var();
+        $this->rt->current_var_is(
+            new Vars\WithName($regexp, $subject));
+
+        return new ExistingVar($this->rt);
     }
 }
