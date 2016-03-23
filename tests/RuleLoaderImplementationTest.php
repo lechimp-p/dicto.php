b2b1a2c4b3e82f6c757a7f921cb43702b2c81777
Richard Klees <richard.klees@rwth-aachen.de>
2016-03-23 20:13:48 +0100

    whitespace

--- tests/RuleLoaderImplementationTest.php
+++ tests/RuleLoaderImplementationTest.php
@@ -1,10 +1,10 @@
 <?php
 /******************************************************************************
  * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
- * 
+ *
  * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de>
  *
- * This software is licensed under The MIT License. You should have received 
+ * This software is licensed under The MIT License. You should have received
  * a copy of the along with the code.
  */
 
@@ -12,7 +12,7 @@ use Lechimp\Dicto as Dicto;
 
 require_once(__DIR__."/RuleLoaderTest.php");
 
-class RuleLoaderImplementationTest extends RuleLoaderTest { 
+class RuleLoaderImplementationTest extends RuleLoaderTest {
     protected function get_rule_loader() {
         return new Dicto\App\Implementation\RuleLoader();
     }
