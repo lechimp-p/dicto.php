d54260937fb2b269c3f37a5466735ffb20c3effe
Richard Klees <richard.klees@rwth-aachen.de>
2016-03-31 21:29:49 +0200

    added test for Rule::checked_on

---
+++ tests/RulesTest.php
@@ -0,0 +1,44 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the along with the code.
+ */
+
+use Lechimp\Dicto\Dicto as Dicto;
+use Lechimp\Dicto\Definition as Def;
+use Lechimp\Dicto\Definition\Rules as Rules;
+use Lechimp\Dicto\Definition\Variables as Vars;
+
+class RulesTest extends PHPUnit_Framework_TestCase {
+    public function test_checked_on() {
+        $rule =
+            new Rules\DependOn
+                ( Rules\Rule::MODE_MUST
+                , new Vars\Classes("CLASSES")
+                , new Vars\Functions("FUNCTIONS")
+                );
+        $expected_checked_on =
+            new Vars\Classes("CLASSES");
+        $this->assertEquals($expected_checked_on, $rule->checked_on());
+    }
+
+    public function test_checked_on_inversion_on_only_can() {
+        $rule =
+            new Rules\Invoke
+                ( Rules\Rule::MODE_ONLY_CAN
+                , new Vars\Classes("CLASSES")
+                , new Vars\Functions("FUNCTIONS")
+                );
+        $expected_checked_on =
+            new Vars\ButNot
+                ( "CHECKED_ON"
+                , new Vars\Everything("EVERYTHING")
+                , new Vars\Classes("CLASSES")
+                );
+        $this->assertEquals($expected_checked_on, $rule->checked_on());
+    }
+}
