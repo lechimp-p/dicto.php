c47a1507eae6dfa239c6ea743ca5f081fca19194
Richard Klees <richard.klees@rwth-aachen.de>
2016-04-19 21:17:19 +0200

    redefined some properties of the stuff the indexer needs to create.

--- src/Indexer/Insert.php
+++ src/Indexer/Insert.php
@@ -19,7 +19,13 @@ interface Insert {
     /**
      * Record general info about an entity.
      *
-     * @param   int             $type   one of Consts::ENTITY_TYPES;
+     * An entity is anything the user defined in its code like a class, a method or
+     * a file, i.e. something where we know for sure how it looks like.
+     *
+     * Uses the same range for ids than reference, that is, each id either referers to
+     * a entity or a reference.
+     *
+     * @param   int             $type       one of Consts::ENTITY_TYPES;
      * @param   string          $name
      * @param   string          $file
      * @param   int             $start_line
@@ -30,11 +36,32 @@ interface Insert {
     public function entity($type, $name, $file, $start_line, $end_line, $source);
 
     /**
+     * Record general info about a reference to an entity.
+     *
+     * A reference to an entity, buildin or global, i.e. a place where we know there
+     * should be something we are refering to by name, but can not get hold of the
+     * source, i.e. a function in a function call or the usage of a global. There
+     * might be the possibility to dereference the reference to an entity later.
+     *
+     * Uses the same range for ids than entity, that is, each id either referers to
+     * a entity or a reference.
+     *
+     * @param   int[]           $possible_types
+     * @param   string          $name
+     * @param   string          $file       where the entity was referenced
+     * @param   int             $line       where the entity was referenced
+     * @return  int                         id of new reference
+     */
+    public function reference($possible_types, $name, $file, $line);
+
+    /**
      * Record information about a dependency.
      *
      * @param   int             $dependent_id
      * @param   int             $dependency_id
-     * @param   int             $source_line
+     * @param   string          $file       where the dependency was created
+     * @param   int             $line       where the dependency was created
+     * @param   string          $source_line
      * @return  null
      */
     public function dependency($dependent_id, $dependency_id, $source_line);
@@ -44,7 +71,9 @@ interface Insert {
      *
      * @param   int             $invoker_id
      * @param   int             $invokee_id
-     * @param   int             $source_line
+     * @param   string          $file       where the invokation was made
+     * @param   int             $line       where the invokation was made
+     * @param   string          $source_line
      * @return  null
      */
     public function invokation($invoker_id, $invokee_id, $source_line);
