0924aaf67ca5aab84bff850192b841fdcc98ebac
Richard Klees <richard.klees@rwth-aachen.de>
2016-04-20 20:50:22 +0200

    made Indexer pass tests. need more.

--- src/Indexer/PhpParser/Indexer.php
+++ src/Indexer/PhpParser/Indexer.php
@@ -50,6 +50,24 @@ class Indexer implements I\Indexer,  \PhpParser\NodeVisitor {
      */
     protected $file_content = null;
 
+    /**
+     * @var int[]|null
+     */
+    protected $dependent_entity_ids = null;
+
+    /**
+     * @var int[]|null
+     */
+    protected $invoker_entity_ids = null;
+
+    /**
+     * This contains the stack of ids were currently in, i.e. the nesting of
+     * known code blocks we are in.
+     *
+     * @var int[]|null
+     */
+    protected $entity_id_stack = null;
+
 
     public function __construct(\PhpParser\Parser $parser) {
         $this->project_root_path = "";
@@ -103,11 +121,18 @@ class Indexer implements I\Indexer,  \PhpParser\NodeVisitor {
         $this->currently_parsing = true;
         $this->file_path = $path;
         $this->file_content = explode("\n", $content);
+        $this-> dependent_entity_ids = array();
+        $this->invoker_entity_ids = array();
     }
 
     private function deinit_parsing_state() {
         assert('$this->currently_parsing');
         $this->currently_parsing = false;
+        $this->file_path = null;
+        $this->file_content = null;
+        $this->dependent_entity_id = null;
+        $this->invoker_entity_ids = null;
+        $this->entity_id_stack = null;
     }
 
 
@@ -115,38 +140,6 @@ class Indexer implements I\Indexer,  \PhpParser\NodeVisitor {
         assert('is_int($start)');
         assert('is_int($end)');
         return implode("\n", array_slice($this->file_content, $start-1, $end-$start+1));
-    }
-
-    // handlers for different actions the parser needs to take
-
-    private function insert_entity_information(\PhpParser\Node $node) {
-        assert('$this->currently_parsing');
-
-        $start_line = $node->getAttribute("startLine");
-        $end_line = $node->getAttribute("endLine");
-        $source = $this->lines_from_to($start_line, $end_line);
-
-        if ($node instanceof N\Stmt\Class_) {
-            $this->insert->entity
-                ( Consts::CLASS_ENTITY
-                , $node->name
-                , $this->file_path
-                , $start_line
-                , $end_line
-                , $source
-                );
-        } 
-        elseif ($node instanceof N\Stmt\ClassMethod
-                or $node instanceof N\Stmt\Function_) {
-            $this->insert->entity
-                ( Consts::FUNCTION_ENTITY
-                , $node->name
-                , $this->file_path
-                , $start_line
-                , $end_line
-                , $source
-                );
-        }
     }
 
     // from \PhpParser\NodeVisitor
@@ -183,7 +176,90 @@ class Indexer implements I\Indexer,  \PhpParser\NodeVisitor {
      */
     public function enterNode(\PhpParser\Node $node) {
         assert('$this->currently_parsing');
-        $this->insert_entity_information($node);
+
+        $start_line = $node->getAttribute("startLine");
+        $end_line = $node->getAttribute("endLine");
+        $source = $this->lines_from_to($start_line, $end_line);
+
+        $id = null;
+
+        // Class
+        if ($node instanceof N\Stmt\Class_) {
+            $id = $this->insert->entity
+                ( Consts::CLASS_ENTITY
+                , $node->name
+                , $this->file_path
+                , $start_line
+                , $end_line
+                , $source
+                );
+
+            // Every interesting reference we find will create a dependency
+            // on this class.
+            $this->dependent_entity_ids[] = $id;
+        }
+        // Method or Function
+        elseif ($node instanceof N\Stmt\ClassMethod
+                or $node instanceof N\Stmt\Function_) {
+            $id = $this->insert->entity
+                ( Consts::FUNCTION_ENTITY
+                , $node->name
+                , $this->file_path
+                , $start_line
+                , $end_line
+                , $source
+                );
+
+            // Every interesting reference we find will create a dependency
+            // on this method or function.
+            $this->dependent_entity_ids[] = $id;
+
+            // Every invocation of a reference we find will be a invocation
+            // in this method or function.
+            $this->invoker_entity_ids[] = $id;
+        }
+        // Call to method or function
+        elseif ($node instanceof N\Expr\MethodCall
+                or $node instanceof N\Expr\FuncCall) {
+            // TODO: we might need to implode parts if we know a thing about
+            // namespaces?
+            $name = $node->name->parts[0];
+
+            $ref_id = $this->insert->reference
+                ( array(Consts::FUNCTION_ENTITY)
+                , $name
+                , $this->file_path
+                , $node->name->getAttribute("startLine")
+                );
+
+            $start_line = $node->getAttribute("startLine");
+            $source_line = $this->lines_from_to($start_line, $start_line);
+            // We are in a class a need to record the dependency now.
+            foreach ($this->dependent_entity_ids as $dependent_id) {
+                $this->insert->dependency
+                    ( $dependent_id
+                    , $ref_id
+                    , $this->file_path
+                    , $start_line
+                    , $source
+                    );
+            }
+            // We also need to record a invocation in every invoking
+            // entity now.
+            foreach ($this->invoker_entity_ids as $invoker_id) {
+                $this->insert->invocation
+                    ( $invoker_id
+                    , $ref_id
+                    , $this->file_path
+                    , $start_line
+                    , $source
+                    );
+            }
+        }
+
+        if ($id !== null) {
+            $this->entity_id_stack[] = $id;
+        }
     }
 
     /**
@@ -191,5 +267,33 @@ class Indexer implements I\Indexer,  \PhpParser\NodeVisitor {
      */
     public function leaveNode(\PhpParser\Node $node) {
         assert('$this->currently_parsing');
+
+        // Class
+        if ($node instanceof N\Stmt\Class_) {
+            // We pushed it, we need to pop it now, as we are leaving the class.
+            $id = array_pop($this->entity_id_stack);
+
+            // Done with the dependencies of this class.
+            assert('in_array($id, $this->dependent_entity_ids)');
+            $key = array_search($id, $this->dependent_entity_ids);
+            unset($this->dependent_entity_ids[$key]);
+        }
+        // Method or Function
+        elseif ($node instanceof N\Stmt\ClassMethod
+                or $node instanceof N\Stmt\Function_) {
+            // We pushed it, we need to pop it now, as we are leaving the method
+            // or function.
+            $id = array_pop($this->entity_id_stack);
+
+            // Done with the dependencies of this method or function.
+            assert('in_array($id, $this->dependent_entity_ids)');
+            $key = array_search($id, $this->dependent_entity_ids);
+            unset($this->dependent_entity_ids[$key]);
+
+            // Done with the invocations in this method or function.
+            assert('in_array($id, $this->invoker_entity_ids)');
+            $key = array_search($id, $this->invoker_entity_ids);
+            unset($this->invoker_entity_ids[$key]);
+        }
     }
 }
