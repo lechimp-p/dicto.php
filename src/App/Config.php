d24c4157cbe44cd5ab10b1c8171cebeec20282d0
Richard Klees <richard.klees@rwth-aachen.de>
2016-05-06 17:11:37 +0200

    added smoke test and simple implementation for config.

--- src/App/Config.php
+++ src/App/Config.php
@@ -15,30 +15,62 @@ namespace Lechimp\Dicto\App;
  */
 class Config {
     /**
-     * @var string
+     * @var string|null
      */
-    protected $project_root;
+    protected $project_root = null;
 
     /**
-     * @var bool 
+     * @var bool|null
      */
-    protected $sqlite_memory; 
+    protected $sqlite_memory = null;
 
     /**
      * @var string|null
      */
-    protected $sqlite_path; 
+    protected $sqlite_path = null;
 
     /**
      * @var string[]
      */
-    protected $analysis_ignore;
+    protected $analysis_ignore = array();
 
     /**
      * Build the configuration from a nested array.
      */
     public function __construct(array $params) {
-            
+        if (isset($params["project"])) {
+            $project = $params["project"];
+            assert('is_array($project)');
+            if (isset($project["root"])) {
+                $this->project_root = $params["project"]["root"];
+                assert('is_string($this->project_root)');
+            }
+        }
+
+        if (isset($params["sqlite"])) {
+            $sqlite = $params["sqlite"];
+            assert('is_array($sqlite)');
+            if (isset($sqlite["memory"])) {
+                $this->sqlite_memory = $sqlite["memory"];
+                assert('is_bool($this->sqlite_memory)');
+            }
+            if (isset($sqlite["path"])) {
+                $this->sqlite_path = $sqlite["path"];
+                assert('is_string($this->sqlite_path)');
+            }
+        }
+
+        if (isset($params["analysis"])) {
+            $analysis = $params["analysis"];
+            assert('is_array($analysis)');
+            if (isset($analysis["ignore"])) {
+                assert('is_array($analysis["ignore"])');
+                $this->analysis_ignore = array_map(function($s) {
+                    assert('is_string($s)');
+                    return $s;
+                }, $analysis["ignore"]);
+            }
+        }
     }
 
     /**
@@ -66,6 +98,6 @@ class Config {
      * @return  string[]
      */
     public function analysis_ignore() {
-        return $this->analysis_ignore();
+        return $this->analysis_ignore;
     }
 }
--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -11,4 +11,25 @@
 use Lechimp\Dicto\App\Config;
 
 class ConfigClassTest extends PHPUnit_Framework_TestCase {
+    public function test_smoke() {
+        $config = new Config(array
+            ( "project" => array
+                ( "root" => "/root/dir"
+                )
+            , "sqlite" => array
+                ( "memory" => true
+                , "path" => "/sqlite/path"
+                )
+            , "analysis" => array
+                ( "ignore" => array
+                    ( ".*\\.omit_me"
+                    )
+                )
+            )
+        );
+        $this->assertEquals("/root/dir", $config->project_root());
+        $this->assertEquals(true, $config->sqlite_memory());
+        $this->assertEquals("/sqlite/path", $config->sqlite_path());
+        $this->assertEquals(array(".*\\.omit_me"), $config->analysis_ignore());
+    }
 }
