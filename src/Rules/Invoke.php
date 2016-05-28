bd476e2910d16e85403008f772aebbc19f5a62cb
Richard Klees <richard.klees@rwth-aachen.de>
2016-05-28 20:22:13 +0200

    refactoring

--- src/Rules/Invoke.php
+++ src/Rules/Invoke.php
@@ -32,37 +32,47 @@ class Invoke extends Relation {
      * @inheritdoc
      */
     public function register_listeners(ListenerRegistry $registry) {
+        $this->register_func_call_listener($registry);
+        $this->register_method_call_listener($registry);
+    }
+
+    protected function register_func_call_listener(ListenerRegistry $registry) {
         $registry->on_enter_misc
-            ( array(N\Expr\FuncCall::class, N\Expr\MethodCall::class)
+            ( array(N\Expr\MethodCall::class)
             , function(Insert $insert, Location $location, \PhpParser\Node $node) {
-                $ref_id = null;
-                if ($node instanceof N\Expr\MethodCall) {
-                    // The 'name' could also be a variable like in $this->$method();
-                    if (is_string($node->name)) {
-                        $ref_id = $insert->get_reference
-                            ( Variable::METHOD_TYPE
-                            , $node->name
-                            , $location->file_path()
-                            , $node->getAttribute("startLine")
-                            );
-                    }
+                // The 'name' could also be a variable like in $this->$method();
+                if (is_string($node->name)) {
+                    $ref_id = $insert->get_reference
+                        ( Variable::METHOD_TYPE
+                        , $node->name
+                        , $location->file_path()
+                        , $node->getAttribute("startLine")
+                        );
+                    $this->insert_relation_into
+                        ( $insert
+                        , $location
+                        , $ref_id
+                        );
                 }
-                elseif($node instanceof N\Expr\FuncCall) {
-                    // Omit calls to closures, we would not be able to
-                    // analyze them anyway atm.
-                    // Omit functions in arrays, we would not be able to
-                    // analyze them anyway atm.
-                    if (!($node->name instanceof N\Expr\Variable ||
-                          $node->name instanceof N\Expr\ArrayDimFetch)) {
-                        $ref_id = $insert->get_reference
-                            ( Variable::FUNCTION_TYPE
-                            , $node->name->parts[0]
-                            , $location->file_path()
-                            , $node->getAttribute("startLine")
-                            );
-                    }
-                }
-                if ($ref_id !== null) {
+            });
+    }
+
+    protected function register_method_call_listener(ListenerRegistry $registry) {
+        $registry->on_enter_misc
+            ( array(N\Expr\FuncCall::class)
+            , function(Insert $insert, Location $location, \PhpParser\Node $node) {
+                // Omit calls to closures, we would not be able to
+                // analyze them anyway atm.
+                // Omit functions in arrays, we would not be able to
+                // analyze them anyway atm.
+                if (!($node->name instanceof N\Expr\Variable ||
+                      $node->name instanceof N\Expr\ArrayDimFetch)) {
+                    $ref_id = $insert->get_reference
+                        ( Variable::FUNCTION_TYPE
+                        , $node->name->parts[0]
+                        , $location->file_path()
+                        , $node->getAttribute("startLine")
+                        );
                     $this->insert_relation_into
                         ( $insert
                         , $location
