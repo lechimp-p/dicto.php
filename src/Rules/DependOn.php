34e726e893835f32c53f8b3164e5b029f637d04d
Richard Klees <richard.klees@rwth-aachen.de>
2016-05-28 20:32:49 +0200

    refactoring

--- src/Rules/DependOn.php
+++ src/Rules/DependOn.php
@@ -33,80 +33,121 @@ class DependOn extends Relation {
      * @inheritdoc
      */
     public function register_listeners(ListenerRegistry $registry) {
+        $this->register_method_call_listener($registry);
+        $this->register_func_call_listener($registry);
+        $this->register_global_listener($registry);
+        $this->register_array_dim_fetch_listener($registry);
+        $this->register_error_suppressor_listener($registry);
+    }
+
+    protected function register_method_call_listener(ListenerRegistry $registry) {
         $registry->on_enter_misc
-            ( array(N\Expr\MethodCall::class, N\Expr\FuncCall::class,
-                    N\Stmt\Global_::class, N\Expr\ArrayDimFetch::class,
-                    N\Expr\ErrorSuppress::class)
+            ( array(N\Expr\MethodCall::class)
             , function(Insert $insert, Location $location, \PhpParser\Node $node) {
-                $ref_ids = array();
-                if ($node instanceof N\Expr\MethodCall) {
-                    // The 'name' could also be a variable like in $this->$method();
-                    if (is_string($node->name)) {
-                        $ref_ids[] = $insert->get_reference
-                            ( Variable::METHOD_TYPE
-                            , $node->name
-                            , $location->file_path()
-                            , $node->getAttribute("startLine")
-                            );
-                    }
-                }
-                elseif($node instanceof N\Expr\FuncCall) {
-                    // Omit calls to closures, we would not be able to
-                    // analyze them anyway atm.
-                    // Omit functions in arrays, we would not be able to
-                    // analyze them anyway atm.
-                    if (!($node->name instanceof N\Expr\Variable ||
-                          $node->name instanceof N\Expr\ArrayDimFetch)) {
-                        $ref_ids[] = $insert->get_reference
-                            ( Variable::FUNCTION_TYPE
-                            , $node->name->parts[0]
-                            , $location->file_path()
-                            , $node->getAttribute("startLine")
-                            );
-                    }
-                }
-                elseif ($node instanceof N\Stmt\Global_) {
-                    foreach ($node->vars as $var) {
-                        if (!($var instanceof N\Expr\Variable) || !is_string($var->name)) {
-                            throw new \RuntimeException(
-                                "Expected Variable with string name, found: ".print_r($var, true));
-                        }
-                        $ref_ids[] = $insert->get_reference
-                            ( Variable::GLOBAL_TYPE
-                            , $var->name
-                            , $location->file_path()
-                            , $node->getAttribute("startLine")
-                            );
-                    }
-                }
-                elseif ($node instanceof N\Expr\ArrayDimFetch) {
-                    if ($node->var instanceof N\Expr\Variable && $node->var->name == "GLOBALS") {
-                        // Ignore usage of $GLOBALS with variable index.
-                        if (!($node->dim instanceof N\Expr\Variable)) {
-                            $ref_ids[] = $insert->get_reference
-                                ( Variable::GLOBAL_TYPE
-                                , $node->dim->value
-                                , $location->file_path()
-                                , $node->getAttribute("startLine")
-                                );
-                        }
-                    }
-                }
-                elseif ($node instanceof N\Expr\ErrorSuppress) {
-                    $ref_ids[] = $insert->get_reference
-                            ( Variable::LANGUAGE_CONSTRUCT_TYPE
-                            , "@"
-                            , $location->file_path()
-                            , $node->getAttribute("startLine")
-                            );
-                }
-                foreach ($ref_ids as $ref_id) {
+                // The 'name' could also be a variable like in $this->$method();
+                if (is_string($node->name)) {
+                    $ref_id = $insert->get_reference
+                        ( Variable::METHOD_TYPE
+                        , $node->name
+                        , $location->file_path()
+                        , $node->getAttribute("startLine")
+                        );
                     $this->insert_relation_into
                         ( $insert
                         , $location
                         , $ref_id
                         );
                 }
+            });
+    }
+
+    protected function register_func_call_listener(ListenerRegistry $registry) {
+        $registry->on_enter_misc
+            ( array(N\Expr\FuncCall::class)
+            , function(Insert $insert, Location $location, \PhpParser\Node $node) {
+                // Omit calls to closures, we would not be able to
+                // analyze them anyway atm.
+                // Omit functions in arrays, we would not be able to
+                // analyze them anyway atm.
+                if (!($node->name instanceof N\Expr\Variable ||
+                      $node->name instanceof N\Expr\ArrayDimFetch)) {
+                    $ref_id = $insert->get_reference
+                        ( Variable::FUNCTION_TYPE
+                        , $node->name->parts[0]
+                        , $location->file_path()
+                        , $node->getAttribute("startLine")
+                        );
+                    $this->insert_relation_into
+                        ( $insert
+                        , $location
+                        , $ref_id
+                        );
+                }
+            });
+    }
+
+    protected function register_global_listener(ListenerRegistry $registry) {
+        $registry->on_enter_misc
+            ( array(N\Stmt\Global_::class)
+            , function(Insert $insert, Location $location, \PhpParser\Node $node) {
+                foreach ($node->vars as $var) {
+                    if (!($var instanceof N\Expr\Variable) || !is_string($var->name)) {
+                        throw new \RuntimeException(
+                            "Expected Variable with string name, found: ".print_r($var, true));
+                    }
+                    $ref_id = $insert->get_reference
+                        ( Variable::GLOBAL_TYPE
+                        , $var->name
+                        , $location->file_path()
+                        , $node->getAttribute("startLine")
+                        );
+                $this->insert_relation_into
+                    ( $insert
+                    , $location
+                    , $ref_id
+                    );
+                }
+            });
+    }
+
+    protected function register_array_dim_fetch_listener(ListenerRegistry $registry) {
+        $registry->on_enter_misc
+            ( array(N\Expr\ArrayDimFetch::class)
+            , function(Insert $insert, Location $location, \PhpParser\Node $node) {
+                if ($node->var instanceof N\Expr\Variable 
+                &&  $node->var->name == "GLOBALS"
+                // Ignore usage of $GLOBALS with variable index.
+                && !($node->dim instanceof N\Expr\Variable)) {
+                    $ref_id = $insert->get_reference
+                        ( Variable::GLOBAL_TYPE
+                        , $node->dim->value
+                        , $location->file_path()
+                        , $node->getAttribute("startLine")
+                        );
+                    $this->insert_relation_into
+                        ( $insert
+                        , $location
+                        , $ref_id
+                        );
+                }
+            });
+    }
+
+    protected function register_error_suppressor_listener(ListenerRegistry $registry) {
+        $registry->on_enter_misc
+            ( array(N\Expr\ErrorSuppress::class)
+            , function(Insert $insert, Location $location, \PhpParser\Node $node) {
+                $ref_id = $insert->get_reference
+                    ( Variable::LANGUAGE_CONSTRUCT_TYPE
+                    , "@"
+                    , $location->file_path()
+                    , $node->getAttribute("startLine")
+                    );
+                $this->insert_relation_into
+                    ( $insert
+                    , $location
+                    , $ref_id
+                    );
             });
         }
 }
