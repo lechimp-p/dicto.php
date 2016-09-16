e823041e19715a20bcd646128f3143b538447cf4
Richard Klees <richard.klees@rwth-aachen.de>
2016-09-17 00:30:51 +0200

    started implementation of queries on a graph.

---
+++ src/Graph/Query.php
@@ -0,0 +1,62 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ * 
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the license along with the code.
+ */
+
+namespace Lechimp\Dicto\Graph;
+
+/**
+ * A query on the graph. Finds specific paths in the graph.
+ */
+class Query {
+    /**
+     * @var Matcher[]
+     */
+    protected $matchers = [];
+
+    /**
+     * Execute the query on a graph. Get a list of results of the
+     * query with the nodes and relations establishing the path.
+     *
+     * @param   Graph   $graph
+     * @return  array[]
+     */
+    public function execute_on(Graph $graph) {
+        $res = array();
+        foreach ($graph->nodes() as $node) {
+            if ($this->matchers[0]->matches($node)) {
+                $res[] = [$node];
+            }
+        }
+        return $res;
+    }
+
+    /**
+     * Get a new query with an additional matcher on an entity.
+     *
+     * @param   Matcher $matcher
+     * @return  Query
+     */
+    public function with_matcher(Matcher $matcher) {
+        $clone = new Query;
+        $clone->matchers = $this->matchers;
+        $clone->matchers[] = $matcher;
+        assert('$this->matchers != $clone->matchers');
+        return $clone;
+    }
+
+    /**
+     * Get a query with the given condition on the next entity.
+     *
+     * @param   \Closure    $condition  Entity -> bool
+     * @param   Query
+     */
+    public function with_condition_on_next_entity(\Closure $condition) {
+        return $this->with_matcher(new Matcher($condition));
+    }
+}
---
+++ tests/GraphQueryTest.php
@@ -0,0 +1,44 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+use Lechimp\Dicto\Graph\Graph;
+use Lechimp\Dicto\Graph\Query;
+
+class GraphQueryTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        $this->g = new Graph(); 
+    }
+
+    public function test_match_all() {
+        $n1 = $this->g->create_node("a_type", []);
+        $n2 = $this->g->create_node("b_type", []);
+
+        $query = (new Query())
+            ->with_condition_on_next_entity(function($_) {
+                return true;
+            });
+        $res = $query->execute_on($this->g);
+
+        $this->assertEquals([[$n1],[$n2]], $res);
+    }
+
+    public function test_match_none() {
+        $n1 = $this->g->create_node("a_type", []);
+        $n2 = $this->g->create_node("b_type", []);
+
+        $query = (new Query())
+            ->with_condition_on_next_entity(function($_) {
+                return false;
+            });
+        $res = $query->execute_on($this->g);
+
+        $this->assertEquals([], $res);
+    }
+}
