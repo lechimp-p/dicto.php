5d6a31f4edea3624790bc1e487636b4fe68ad3c8
Richard Klees <richard.klees@rwth-aachen.de>
2016-09-17 01:00:49 +0200

    implemented tests with real matching.

--- tests/GraphQueryTest.php
+++ tests/GraphQueryTest.php
@@ -56,4 +56,40 @@ class GraphQueryTest extends PHPUnit_Framework_TestCase {
         $res = $query->execute_on($this->g);
         $this->assertEquals([[$n1,$rel,$n2]], $res);
     }
+
+    public function test_path2() {
+        $n1 = $this->g->create_node("a_type", []);
+        $n2 = $this->g->create_node("b_type", []);
+        $n3 = $this->g->create_node("c_type", []);
+        $r1 = $this->g->add_relation($n1, "rel_A", [], $n2);
+        $r2 = $this->g->add_relation($n2, "rel_B", [], $n3);
+
+        $all = function($_) { return true; };
+
+        $query = (new Query())
+            ->with_condition($all)
+            ->with_condition(function($e) {
+                return $e->type() == "rel_B";
+            })
+            ->with_condition($all);
+        $res = $query->execute_on($this->g);
+        $this->assertEquals([[$n2,$r2,$n3]], $res);
+    }
+
+    public function test_paths() {
+        $n1 = $this->g->create_node("a_type", []);
+        $n2 = $this->g->create_node("b_type", []);
+        $n3 = $this->g->create_node("c_type", []);
+        $r1 = $this->g->add_relation($n1, "rel_A", [], $n2);
+        $r2 = $this->g->add_relation($n2, "rel_A", [], $n3);
+
+        $all = function($_) { return true; };
+
+        $query = (new Query())
+            ->with_condition($all)
+            ->with_condition($all)
+            ->with_condition($all);
+        $res = $query->execute_on($this->g);
+        $this->assertEquals([[$n1,$r1,$n2],[$n2,$r2,$n3]], $res);
+    }
 }
