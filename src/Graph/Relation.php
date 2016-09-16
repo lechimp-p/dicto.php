c093fb22fa26eb564cacec48f9f56540eabe61bc
Richard Klees <richard.klees@rwth-aachen.de>
2016-09-16 23:14:24 +0200

    added relation between two nodes to graph.

---
+++ src/Graph/Relation.php
@@ -0,0 +1,35 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ * 
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the license along with the code.
+ */
+
+namespace Lechimp\Dicto\Graph;
+
+/**
+ * A relation between two nodes in the Graph. It is unidirectional.
+ */
+class Relation extends Entity {
+    /**
+     * @param   string              $type
+     * @param   array<string,mixed> $properties
+     * @param   Node                $target 
+     */
+    public function __construct($type, $properties, Node $target) {
+        parent::__construct($type, $properties);
+        $this->target = $target;
+    }
+
+    /**
+     * The target of the relation.
+     *
+     * @return  Node
+     */
+    public function target() {
+        return $this->target;
+    }
+}
---
+++ tests/GraphRelationTest.php
@@ -0,0 +1,32 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+use Lechimp\Dicto\Graph\Relation;
+use Lechimp\Dicto\Graph\Node;
+
+class GraphRelationTest extends PHPUnit_Framework_TestCase {
+    public function test_type() {
+        $t = new Node(0, "some_type", []);
+        $e = new Relation("a_type", array(), $t);
+        $this->assertEquals("a_type", $e->type());
+    }
+
+    public function test_properties() {
+        $t = new Node(0, "some_type", []);
+        $e = new Relation("a_type", ["prop" => "value"], $t);
+        $this->assertEquals(["prop" => "value"], $e->properties());
+    }
+
+    public function test_target() {
+        $t = new Node(0, "some_type", []);
+        $e = new Relation("a_type", ["prop" => "value"], $t);
+        $this->assertSame($t, $e->target());
+    }
+}
