834795ab7868e4ae4b37ad9a82246e767ed31213
Richard Klees <richard.klees@rwth-aachen.de>
2016-09-25 17:25:53 +0200

    added specific query interface for index.

---
+++ src/Graph/IndexQuery.php
@@ -0,0 +1,66 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+namespace Lechimp\Dicto\Graph;
+
+/**
+ * A query on the IndexDB.
+ */
+interface IndexQuery extends Query {
+    /**
+     * Get files only.
+     *
+     * @return Query
+     */
+    public function files();
+
+    /**
+     * Get classes only.
+     *
+     * @return Query
+     */
+    public function classes();
+
+    /**
+     * Get methods only.
+     *
+     * @return Query
+     */
+    public function methods();
+
+    /**
+     * Get functions only.
+     *
+     * @return Query
+     */
+    public function functions();
+
+    /**
+     * Get globals only.
+     *
+     * @return Query
+     */
+    public function globals();
+
+    /**
+     * Expand to relations with given types.
+     *
+     * @param   string[]    $types
+     * @return  Query
+     */
+    public function expand_relation(array $types);
+
+    /**
+     * Expand to the targets of the relations.
+     *
+     * @return  Query
+     */
+    public function expand_target();
+}
---
+++ src/Graph/IndexQueryImpl.php
@@ -0,0 +1,78 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+namespace Lechimp\Dicto\Graph;
+
+/**
+ * A query on the IndexDB.
+ */
+class IndexQueryImpl extends QueryImpl implements IndexQuery {
+    protected function type_filter($type) {
+        return function(Node $n) use ($type) {
+            return $n->type() == $type;
+        };
+    }
+
+    /**
+     */
+    public function files() {
+        return $this->filter($this->type_filter("file"));
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function classes() {
+        return $this->filter($this->type_filter("class"));
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function methods() {
+        return $this->filter($this->type_filter("method"));
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function functions() {
+        return $this->filter($this->type_filter("function"));
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function globals() {
+        return $this->filter($this->type_filter("global"));
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function expand_relation(array $types) {
+        return $this->expand(function(Node $n) use (&$types) {
+            return array_filter
+                ( $n->relations()
+                , function(Relation $r) use (&$types) {
+                    return in_array($r->type(), $types);
+                });
+        });
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function expand_target() {
+        return $this->expand(function(Relation $r) {
+            return [$r->target()];
+        });
+    }
+}
