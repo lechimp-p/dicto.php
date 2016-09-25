5bbfb95cdb22d0c55660056a3edb43034d2e1baf
Richard Klees <richard.klees@rwth-aachen.de>
2016-09-25 17:26:23 +0200

    started to implement new Insert interface on Graph/IndexDB

--- src/Graph/IndexDB.php
+++ src/Graph/IndexDB.php
@@ -17,94 +17,139 @@ use Lechimp\Dicto\Indexer\Insert;
  */
 class IndexDB extends Graph implements Insert {
     /**
+     * @var array<string,Node>
+     */
+    protected $globals = array();
+
+    /**
      * @inheritdocs
      */
-    public function name($name, $type) {
-        $res = (new QueryImpl)
-            ->with_filter(function(Node $n) use ($name) {
-                return $n->type() == "name"
-                    && $n->property("name") == $name;
-            })
-            ->execute_on($this);
-        if ($res->is_empty()) {
-            return $this->create_node("name", ["name" => $name, "type" => $type])->id();
+    public function _file($path, $source) {
+        assert('is_string($path)');
+        assert('is_string($source)');
+        return $this->create_node
+            ( "file"
+            ,   [ "path" => $path
+                , "source" => explode("\n", $source)
+                ]
+            );
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function _class($name, $file, $start_line, $end_line) {
+        assert('is_string($name)');
+        assert('$file->type() == "file"');
+        assert('is_int($start_line)');
+        assert('is_int($end_line)');
+        $class = $this->create_node
+            ( "class"
+            ,   [ "name" => $name
+                ]
+            );
+        $this->add_relation
+            ( $class
+            , "defined in"
+            ,   [ "start_line" => $start_line
+                , "end_line" => $end_line
+                ]
+            , $file
+            );
+        return $class;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function _method($name, $class, $file, $start_line, $end_line) {
+        assert('is_string($name)');
+        assert('$class->type() == "class"');
+        assert('$file->type() == "file"');
+        assert('is_int($start_line)');
+        assert('is_int($end_line)');
+        $method = $this->create_node
+            ( "method"
+            ,   [ "name" => $name
+                ]
+            );
+        $this->add_relation
+            ( $method
+            , "defined in"
+            ,   [ "start_line" => $start_line
+                , "end_line" => $end_line
+                ]
+            , $file
+            );
+        $this->add_relation($method, "contained in", [], $class);
+        $this->add_relation($class, "contains", [], $method);
+
+        return $method;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function _function($name, $file, $start_line, $end_line) {
+        assert('is_string($name)');
+        assert('$file->type() == "file"');
+        assert('is_int($start_line)');
+        assert('is_int($end_line)');
+        $method = $this->create_node
+            ( "function"
+            ,   [ "name" => $name
+                ]
+            );
+        $this->add_relation
+            ( $method
+            , "defined in"
+            ,   [ "start_line" => $start_line
+                , "end_line" => $end_line
+                ]
+            , $file
+            );
+
+        return $method;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function _global($name) {
+        if (array_key_exists($name, $this->globals)) {
+            return $this->globals[$name];
         }
-        $res = $res
-            ->extract(function ($n) {
-                return $n->id();
-            });
-        assert('count($res) == 1');
-        return $res[0];
+        $global = $this->create_node("global", ["name" => $name]);
+        $this->globals[$name] = $global;
+        return $global;
     }
 
     /**
      * @inheritdocs
      */
-    public function file($path) {
-        $res = (new QueryImpl)
-            ->with_filter(function(Node $n) use ($path) {
-                return $n->type() == "file"
-                    && $n->property("path") == $path;
-            })
-            ->execute_on($this);
-
-        if ($res->is_empty()) {
-            return $this->create_node("file", ["path" => $path])->id();
-        }
-        $res = $res
-            ->extract(function ($n) {
-                return $n->id();
-            });
-        assert('count($res) == 1');
-        return $res[0];
-    }
+    public function _language_construct($name, $file, $line) {}
 
     /**
      * @inheritdocs
      */
-    public function source($path, $content) {
-        assert('is_string($content)');
-        $file_id = $this->file($path);
-        $file = $this->node($file_id);
-        $num = 1;
-        foreach (explode("\n", $content) as $source) {
-            $line = $this->create_node("line", ["num" => $num, "source" => $source]);
-            $this->add_relation($file, "contains", [], $line);
-            $num++;
-        }
-        return $file_id;
-    }
+    public function _method_reference($name, $file, $line) {}
 
     /**
      * @inheritdocs
      */
-    public function definition($name, $type, $file, $start_line, $end_line) {
-        $name_id = $this->name($name, $type);
-        $name = $this->node($name_id);
-        $file_id = $this->file($file);
-        $file = $this->node($file_id);
-        $definition = $this->create_node("definition",
-            ["start_line" => $start_line, "end_line" => $end_line]);
-        $this->add_relation($name, "has_definition", [], $definition);
-        $this->add_relation($definition, "in_file", [], $file);
-        return array($name_id, $definition->id());
-    }
+    public function _function_reference($name, $file, $line) {}
 
     /**
      * @inheritdocs
      */
-    public function method_info($name_id, $class_name_id, $definition_id) {
-        $name = $this->node($name_id);
-        $class_name = $this->node($class_name_id);
-        $this->add_relation($name, "in_class", ["def_id" => $definition_id], $class_name);
-    }
+    public function _relation($left_entity, $right_entity, $file, $line) {}
 
     /**
-     * @inheritdocs
+     * Build a query on the index.
+     *
+     * @return  IndexQuery
      */
-     public function relation($name_left_id, $name_right_id, $which, $file, $line) {
-        $name_left = $this->node($name_left_id);
-        $name_right = $this->node($name_right_id);
-        $this->add_relation($name_left, $which, ["path" => $file, "line" => $line], $name_right);
-     }
+    public function query() {
+        return new IndexQueryImpl($this);
+    }
 }
--- tests/GraphIndexDBTest.php
+++ tests/GraphIndexDBTest.php
@@ -14,12 +14,196 @@ use Lechimp\Dicto\Graph\_Query;
 use Lechimp\Dicto\Graph\Node;
 use Lechimp\Dicto\Graph\Relation;
 
-abstract class GraphIndexDBTest extends PHPUnit_Framework_TestCase {
+class GraphIndexDBTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
         $this->db = new IndexDB();
     }
 
-    public function test_insert_source() {
+    public function test_file() {
+        $this->db->_file("some_path.php", "A\nB");
+
+        $res = $this->db->query()
+            ->files()
+            ->extract(function($n, &$r) {
+                $r["path"] = $n->property("path");
+                $r["source"] = $n->property("source");
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "path" => "some_path.php"
+                , "source" => ["A","B"]
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_class() {
+        $file = $this->db->_file("some_path.php", "A\nB");
+        $this->db->_class("AClass", $file, 1, 2);
+
+        $res = $this->db->query()
+            ->classes()
+            ->extract(function($n, &$r) {
+                $r["name"] = $n->property("name");
+            })
+            ->expand_relation(["defined in"])
+            ->extract(function($e,&$r) {
+                $r["start_line"] = $e->property("start_line");
+                $r["end_line"] = $e->property("end_line");
+            })
+            ->expand_target()
+            ->extract(function($e,&$r) {
+                $r["file"] = $e;
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "name" => "AClass"
+                , "file" => $file
+                , "start_line" => 1
+                , "end_line" => 2
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_method() {
+        $file = $this->db->_file("some_path.php", "A\nB\nC\nD");
+        $class = $this->db->_class("AClass", $file, 1, 4);
+        $this->db->_method("a_method", $class, $file, 2, 3);
+
+        $res = $this->db->query()
+            ->methods()
+            ->extract(function($n, &$r) {
+                $r["name"] = $n->property("name");
+                $r["class"] = $n->related_nodes(function($r) {
+                    return $r->type() == "contained in";
+                });
+            })
+            ->expand_relation(["defined in"])
+            ->extract(function($e,&$r) {
+                $r["start_line"] = $e->property("start_line");
+                $r["end_line"] = $e->property("end_line");
+            })
+            ->expand_target()
+            ->extract(function($e,&$r) {
+                $r["file"] = $e;
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "name" => "a_method"
+                , "class" => [$class]
+                , "file" => $file
+                , "start_line" => 2
+                , "end_line" => 3
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_method_class_rel() {
+        $file = $this->db->_file("some_path.php", "A\nB\nC\nD");
+        $class = $this->db->_class("AClass", $file, 1, 4);
+        $method = $this->db->_method("a_method", $class, $file, 2, 3);
+
+        $res = $this->db->query()
+            ->classes()
+            ->expand_relation(["contains"])
+            ->expand_target()
+            ->extract(function($n, &$r) {
+                $r["method"] = $n;
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "method" => $method
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_function() {
+        $file = $this->db->_file("some_path.php", "A\nB\nC\nD");
+        $this->db->_function("a_function", $file, 2, 3);
+
+        $res = $this->db->query()
+            ->functions()
+            ->extract(function($n, &$r) {
+                $r["name"] = $n->property("name");
+            })
+            ->expand_relation(["defined in"])
+            ->extract(function($e,&$r) {
+                $r["start_line"] = $e->property("start_line");
+                $r["end_line"] = $e->property("end_line");
+            })
+            ->expand_target()
+            ->extract(function($e,&$r) {
+                $r["file"] = $e;
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "name" => "a_function"
+                , "file" => $file
+                , "start_line" => 2
+                , "end_line" => 3
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_global() {
+        $this->db->_global("some_global");
+
+        $res = $this->db->query()
+            ->globals()
+            ->extract(function($n,&$r) {
+                $r["name"] = $n->property("name");
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "name" => "some_global"
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_insert_global_twice() {
+        $ga = $this->db->_global("some_global");
+        $gb = $this->db->_global("some_global");
+
+        $res = $this->db->query()
+            ->globals()
+            ->extract(function($n,&$r) {
+                $r["name"] = $n->property("name");
+            })
+            ->run([]);
+
+        $expected =
+            [   [ "name" => "some_global"
+                ]
+            ];
+        $this->assertEquals($expected, $res);
+        $this->assertSame($ga, $gb);
+    }
+
+    public function test_language_construct() {
+        // TODO: implement this
+    }
+
+    public function test_method_reference() {
+    }
+
+    public function test_function_reference() {
+    }
+
+    public function test_relation() {
+    }
+
+/*    public function test_insert_source() {
         $this->db->source("foo.php", "FOO\nBAR");
 
         $res = (new _Query)
@@ -370,5 +554,5 @@ abstract class GraphIndexDBTest extends PHPUnit_Framework_TestCase {
                 ]
             ];
         $this->assertEquals($expected, $res);
-    }
+    }*/
 }
