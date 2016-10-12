1f92e69c48f36c52bbb03aba6747317eef6f2e1f
Richard Klees <richard.klees@rwth-aachen.de>
2016-10-12 20:25:48 +0200

    these might be some usefull tests on contain text i wrote once...

--- tests/ContainTextTest.php
+++ tests/ContainTextTest.php
@@ -188,5 +188,92 @@ CODE;
         $this->assertEquals($expected, $violations);
     }
 
+    // RULE 3
+
+    protected function files_cannot_contain_text_foo() {
+        return new R\Rule
+            ( R\Rule::MODE_CANNOT
+            , new V\Files()
+            , new R\ContainText()
+            , array("foo")
+            );
+    }
+
+    public function test_rule3_no_violation_1() {
+        $rule = $this->files_cannot_contain_text_foo();
+        $code = <<<CODE
+<?php
+
+class BClass {
+    public function a_method() {
+        global \$bar;
+    }
+}
+
+CODE;
+
+        $violations = $this->analyze($rule, $code);
+        $this->assertCount(0, $violations);
+    }
+
+    public function test_rule3_no_violation_2() {
+        $rule = $this->files_cannot_contain_text_foo();
+        $code = <<<CODE
+<?php
+
+class AClass {
+    public function a_method() {
+    }
+}
+
+CODE;
+
+        $violations = $this->analyze($rule, $code);
+        $this->assertCount(0, $violations);
+    }
+
+    public function test_rule3_violation_1() {
+        $rule = $this->files_cannot_contain_text_foo();
+        $code = <<<CODE
+<?php
+
+class AClass {
+    public function a_method() {
+        global \$foo;
+    }
+}
+
+CODE;
+
+        $violations = $this->analyze($rule, $code);
+        $expected = array
+            ( new Violation
+                ( $rule
+                , "source.php"
+                , 5
+                , "        global \$foo;"
+                )
+            );
+        $this->assertEquals($expected, $violations);
+    }
+
+    public function test_rule3_violation_2() {
+        $rule = $this->files_cannot_contain_text_foo();
+        $code = <<<CODE
+foo
+CODE;
+
+        $violations = $this->analyze($rule, $code);
+        $expected = array
+            ( new Violation
+                ( $rule
+                , "source.php"
+                , 1
+                , "foo"
+                )
+            );
+        $this->assertEquals($expected, $violations);
+    }
+
     // TODO: add a test on the regexp
 }
