5192f133685633da755d8732528bbe39c9fb63c6
Richard Klees <richard.klees@rwth-aachen.de>
2016-10-14 22:55:32 +0200

    added main script

---
+++ dicto.php
@@ -0,0 +1,21 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+require_once(__DIR__."/vendor/autoload.php");
+
+$app = new \Lechimp\Dicto\App\App();
+
+$start_time = microtime(true);
+$app->run($_SERVER["argv"]);
+$time_elapsed_secs = microtime(true) - $start_time;
+
+echo "execution time:    ".$time_elapsed_secs."s\n";
+echo "peak memory usage: ".number_format(memory_get_peak_usage(), 0, ".", ".")." byte\n";
+
