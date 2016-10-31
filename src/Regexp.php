ad7265cff8b926726d78fef1e372a3bf1c479572
Richard Klees <richard.klees@rwth-aachen.de>
2016-10-31 12:32:49 +0100

    added Regexp class

---
+++ src/Regexp.php
@@ -0,0 +1,86 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ * 
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the license along with the code.
+ */
+
+namespace Lechimp\Dicto;
+
+/**
+ * Small wrapper around preg.
+ */
+class Regexp {
+    /**
+     * @var string
+     */
+    protected $regexp;
+
+    /**
+     * @var string
+     */
+    protected $delim = "%";
+
+    public function __construct($regexp) {
+        assert('is_string($regexp)');
+        if (@preg_match($this->delim.$regexp.$this->delim, "") === false) {
+            throw new \InvalidArgumentException("Invalid regexp '$regexp'");
+        }
+        $this->regexp = $regexp;
+    }
+
+    /**
+     * Match a string with the regexp.
+     *
+     * @param   string      $str
+     * @param   bool        $dotall
+     * @param   array|null  $matches
+     * @return  bool
+     */
+    public function match($str, $dotall = false, &$matches = null) {
+        if (!$dotall) {
+            return preg_match($this->delim."^".$this->regexp.'$'.$this->delim, $str, $matches) === 1;
+        }
+        else {
+            return preg_match($this->delim."^".$this->regexp.'$'.$this->delim."s", $str, $matches) === 1;
+        }
+    }
+
+    /**
+     * Match the beginning of a string with the regexp.
+     *
+     * @param   string      $str
+     * @param   bool        $dotall
+     * @param   array|null  $matches
+     * @return  bool
+     */
+    public function match_beginning($str, $dotall = false, &$matches = null) {
+        if (!$dotall) {
+            return preg_match($this->delim."^".$this->regexp.$this->delim, $str, $matches) === 1;
+        }
+        else {
+            return preg_match($this->delim."^".$this->regexp.$this->delim."s", $str, $matches) === 1;
+        }
+    }
+
+    /**
+     * Search a string with the regexp.
+     *
+     * @param   string      $str
+     * @param   bool        $dotall
+     * @param   array|null  $matches
+     * @return  bool
+     */
+    public function search($str, $dotall = false, &$matches = null) {
+        if (!$dotall) {
+            return preg_match($this->delim.$this->regexp.$this->delim, $str, $matches) === 1;
+        }
+        else {
+            return preg_match($this->delim.$this->regexp.$this->delim."s", $str, $matches) === 1;
+        }
+    }
+}
+
---
+++ tests/RegexpTest.php
@@ -0,0 +1,82 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ * 
+ * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the license along with the code.
+ */
+
+use Lechimp\Dicto\Regexp;
+
+class RegexpTest extends PHPUnit_Framework_TestCase {
+    public function regexp($str) {
+        return new Regexp($str);
+    }
+
+    public function test_throws_on_delim() {
+        try {
+            $this->regexp("%");
+            $this->assertFalse("This should not happen.");
+        }
+        catch (\InvalidArgumentException $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function test_match() {
+        $re = $this->regexp("ab");
+        $this->assertTrue($re->match("ab"));
+        $this->assertFalse($re->match("abc"));
+        $this->assertFalse($re->match("0abc"));
+        $this->assertFalse($re->match("0ab"));
+        $this->assertFalse($re->match("cd"));
+    }
+
+    public function test_match_beginning() {
+        $re = $this->regexp("ab");
+        $this->assertTrue($re->match_beginning("ab"));
+        $this->assertTrue($re->match_beginning("abc"));
+        $this->assertFalse($re->match_beginning("0abc"));
+        $this->assertFalse($re->match_beginning("0ab"));
+        $this->assertFalse($re->match_beginning("cd"));
+    }
+
+    public function test_search() {
+        $re = $this->regexp("ab");
+        $this->assertTrue($re->search("ab"));
+        $this->assertTrue($re->search("abc"));
+        $this->assertTrue($re->search("0abc"));
+        $this->assertTrue($re->search("0ab"));
+        $this->assertFalse($re->search("cd"));
+    }
+
+    public function test_matches() {
+        $re = $this->regexp("(a)(b)");
+
+        $matches = array();
+        $re->match("ab", false, $matches);
+        $this->assertEquals(["ab", "a", "b"], $matches);
+
+        $matches = array();
+        $re->match_beginning("ab", false, $matches);
+        $this->assertEquals(["ab", "a", "b"], $matches);
+
+        $matches = array();
+        $re->search("ab", false, $matches);
+        $this->assertEquals(["ab", "a", "b"], $matches);
+    }
+
+    public function test_dotall() {
+        $re = $this->regexp("(a).(b)");
+
+        $this->assertFalse($re->match("a\nb"));
+        $this->assertFalse($re->match_beginning("a\nb"));
+        $this->assertFalse($re->search("a\nb"));
+
+        $this->assertTrue($re->match("a\nb", true));
+        $this->assertTrue($re->match_beginning("a\nb", true));
+        $this->assertTrue($re->search("a\nb", true));
+    }
+}
