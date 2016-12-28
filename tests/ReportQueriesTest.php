568b85f54c01c9665540c880449a759367be9564
Richard Klees <richard.klees@rwth-aachen.de>
2016-12-28 21:07:07 +0100

    factored out some base class for report tests.

--- tests/ReportQueriesTest.php
+++ tests/ReportQueriesTest.php
@@ -8,55 +8,9 @@
  * a copy of the license along with the code.
  */
 
-use Lechimp\Dicto\Report\ResultDB;
-use Lechimp\Dicto\Report\Queries;
-use Lechimp\Dicto\Rules as Rules;
-use Lechimp\Dicto\Variables as Vars;
-use Lechimp\Dicto\Analysis\Violation;
+require_once(__DIR__."/ReportTestBase.php");
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\DriverManager;
-
-class ReportQueriesTest extends PHPUnit_Framework_TestCase {
-    public function setUp() {
-        $this->connection = DriverManager::getConnection
-            ( array
-                ( "driver" => "pdo_sqlite"
-                //, "path" => "/home/lechimp/dt.sqlite"
-                , "memory" => true
-                )
-            );
-        $this->db = new ResultDB($this->connection);
-        $this->db->init_database_schema();
-        $this->queries = new Queries($this->db);
-    }
-
-    protected function builder() {
-        return $this->connection->createQueryBuilder();
-    }
-
-    // Some example rules
-
-    public function all_classes_cannot_depend_on_globals() {
-        return new Rules\Rule
-            ( Rules\Rule::MODE_CANNOT
-            , new Vars\Classes("allClasses")
-            , new Rules\DependOn()
-            , array(new Vars\Globals("allGlobals"))
-            );
-    }
-
-    public function all_classes_cannot_invoke_functions() {
-        return new Rules\Rule
-            ( Rules\Rule::MODE_CANNOT
-            , new Vars\Classes("allClasses")
-            , new Rules\Invoke()
-            , array(new Vars\Functions("allFunctions"))
-            );
-    }
-
-    // Actual Tests
-
+class ReportQueriesTest extends ReportTestBase {
     public function test_current_run() {
         $this->db->begin_run("#COMMIT_HASH#");
         $this->db->end_run();
@@ -131,77 +85,6 @@ class ReportQueriesTest extends PHPUnit_Framework_TestCase {
         $info = $this->queries->run_info($run);
  
         $this->assertEquals($hash, $info["commit_hash"]);
-    }
-
-    protected function init_scenario() {
-        // Some scenario containing two rules, three runs
-        // and different violations.
-        $rule1 = $this->all_classes_cannot_depend_on_globals();
-        $rule2 = $this->all_classes_cannot_invoke_functions();
-        $vars = array_merge($rule1->variables(), $rule2->variables());
-        $ruleset = new Rules\Ruleset($vars, [$rule1, $rule2]);
-
-        $commit1 = "#COMMIT_1#";
-        // One violation of rule1, no violation of rule2
-        $this->db->begin_run($commit1);
-        $this->db->begin_ruleset($ruleset);
-        $this->db->begin_rule($rule1);
-        $this->db->report_violation(
-            new Violation($rule1, "file.php", 42, "file.php_line_42"));
-        $this->db->end_rule();
-        $this->db->end_ruleset();
-        $this->db->end_run();
-
-        $commit2 = "#COMMIT_2#";
-        // One new violation of rule1, previous violation still exists.
-        // One new violation of rule2.
-        $this->db->begin_run($commit2);
-        $this->db->begin_ruleset($ruleset);
-        $this->db->begin_rule($rule1);
-        $this->db->report_violation(
-            new Violation($rule1, "file.php", 42, "file.php_line_42"));
-        $this->db->report_violation(
-            new Violation($rule1, "file2.php", 23, "file2.php_line_23"));
-        $this->db->end_rule();
-        $this->db->begin_rule($rule2);
-        $this->db->report_violation(
-            new Violation($rule2, "file3.php", 13, "file2.php_line_13"));
-        $this->db->end_rule();
-        $this->db->end_ruleset();
-        $this->db->end_run();
-
-        $commit3 = "#COMMIT_3#";
-        // All violations of rule1 resolved.
-        // Two new violation of rule2, previous violation resolved.
-        $this->db->begin_run($commit3);
-        $this->db->begin_ruleset($ruleset);
-        $this->db->begin_rule($rule1);
-        $this->db->end_rule();
-        $this->db->begin_rule($rule2);
-        $this->db->report_violation(
-            new Violation($rule2, "file.php", 42, "file.php_line_42"));
-        $this->db->report_violation(
-            new Violation($rule2, "file2.php", 23, "file2.php_line_23"));
-        $this->db->end_rule();
-        $this->db->end_ruleset();
-        $this->db->end_run();
-
-        $this->rule1 = $rule1;
-        $this->rule2 = $rule2;
-    }
-
-    // Helper to query ids of known rules.
-    public function query_rule_ids() {
-        $run = $this->queries->last_run_for("#COMMIT_3#");
-        $rules = $this->queries->analyzed_rules($run);
-        $info = $this->queries->rule_info($rules[0]);
-
-        if ($info["rule"] == $this->rule1->pprint()) {
-            return [$rules[0], $rules[1]];
-        }
-        else {
-            return [$rules[1], $rules[0]];
-        }
     }
 
     public function test_analyzed_rules() {
@@ -398,11 +281,4 @@ class ReportQueriesTest extends PHPUnit_Framework_TestCase {
 
     // TODO: Check what happens if a violation is first found, then resolved,
     // then introduced again.
-
-    /*
-        $this->db->begin_rule($this->all_classes_cannot_depend_on_globals());
-        $rule = $this->all_classes_cannot_depend_on_globals();
-        $violation = new Violation($rule, "file.php", 42, "line of code");
-        $this->db->report_violation($violation);
-    */
 }
---
+++ tests/ReportTestBase.php
@@ -0,0 +1,124 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+use Lechimp\Dicto\Report\ResultDB;
+use Lechimp\Dicto\Report\Queries;
+use Lechimp\Dicto\Rules as Rules;
+use Lechimp\Dicto\Variables as Vars;
+use Lechimp\Dicto\Analysis\Violation;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+
+class ReportTestBase extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        $this->connection = DriverManager::getConnection
+            ( array
+                ( "driver" => "pdo_sqlite"
+                //, "path" => "/home/lechimp/dt.sqlite"
+                , "memory" => true
+                )
+            );
+        $this->db = new ResultDB($this->connection);
+        $this->db->init_database_schema();
+        $this->queries = new Queries($this->db);
+    }
+
+    // Some example rules
+
+    public function all_classes_cannot_depend_on_globals() {
+        return new Rules\Rule
+            ( Rules\Rule::MODE_CANNOT
+            , new Vars\Classes("allClasses")
+            , new Rules\DependOn()
+            , array(new Vars\Globals("allGlobals"))
+            );
+    }
+
+    public function all_classes_cannot_invoke_functions() {
+        return new Rules\Rule
+            ( Rules\Rule::MODE_CANNOT
+            , new Vars\Classes("allClasses")
+            , new Rules\Invoke()
+            , array(new Vars\Functions("allFunctions"))
+            );
+    }
+
+    protected function init_scenario() {
+        // Some scenario containing two rules, three runs
+        // and different violations.
+        $rule1 = $this->all_classes_cannot_depend_on_globals();
+        $rule2 = $this->all_classes_cannot_invoke_functions();
+        $vars = array_merge($rule1->variables(), $rule2->variables());
+        $ruleset = new Rules\Ruleset($vars, [$rule1, $rule2]);
+
+        $commit1 = "#COMMIT_1#";
+        // One violation of rule1, no violation of rule2
+        $this->db->begin_run($commit1);
+        $this->db->begin_ruleset($ruleset);
+        $this->db->begin_rule($rule1);
+        $this->db->report_violation(
+            new Violation($rule1, "file.php", 42, "file.php_line_42"));
+        $this->db->end_rule();
+        $this->db->end_ruleset();
+        $this->db->end_run();
+
+        $commit2 = "#COMMIT_2#";
+        // One new violation of rule1, previous violation still exists.
+        // One new violation of rule2.
+        $this->db->begin_run($commit2);
+        $this->db->begin_ruleset($ruleset);
+        $this->db->begin_rule($rule1);
+        $this->db->report_violation(
+            new Violation($rule1, "file.php", 42, "file.php_line_42"));
+        $this->db->report_violation(
+            new Violation($rule1, "file2.php", 23, "file2.php_line_23"));
+        $this->db->end_rule();
+        $this->db->begin_rule($rule2);
+        $this->db->report_violation(
+            new Violation($rule2, "file3.php", 13, "file2.php_line_13"));
+        $this->db->end_rule();
+        $this->db->end_ruleset();
+        $this->db->end_run();
+
+        $commit3 = "#COMMIT_3#";
+        // All violations of rule1 resolved.
+        // Two new violation of rule2, previous violation resolved.
+        $this->db->begin_run($commit3);
+        $this->db->begin_ruleset($ruleset);
+        $this->db->begin_rule($rule1);
+        $this->db->end_rule();
+        $this->db->begin_rule($rule2);
+        $this->db->report_violation(
+            new Violation($rule2, "file.php", 42, "file.php_line_42"));
+        $this->db->report_violation(
+            new Violation($rule2, "file2.php", 23, "file2.php_line_23"));
+        $this->db->end_rule();
+        $this->db->end_ruleset();
+        $this->db->end_run();
+
+        $this->rule1 = $rule1;
+        $this->rule2 = $rule2;
+    }
+
+    // Helper to query ids of known rules.
+    public function query_rule_ids() {
+        $run = $this->queries->last_run_for("#COMMIT_3#");
+        $rules = $this->queries->analyzed_rules($run);
+        $info = $this->queries->rule_info($rules[0]);
+
+        if ($info["rule"] == $this->rule1->pprint()) {
+            return [$rules[0], $rules[1]];
+        }
+        else {
+            return [$rules[1], $rules[0]];
+        }
+    }
+}
