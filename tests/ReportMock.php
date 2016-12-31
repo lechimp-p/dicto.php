e04373cd3e007655769b56514bf687a85388e7b0
Richard Klees <richard.klees@rwth-aachen.de>
2016-12-31 16:35:03 +0100

    made constructor compatible to Report

--- tests/ReportMock.php
+++ tests/ReportMock.php
@@ -13,8 +13,11 @@ use Lechimp\Dicto\Report\Report;
 class ReportMock extends Report {
     public $data = [];
 
-    public function __construct(array $config = []) {
+    public function __construct($queries = null, array $config = []) {
         $this->config = $config;
+        if (isset($config["data"])) {
+            $this->data = $config["data"];
+        }
     }
 
     protected function default_template_path() {
