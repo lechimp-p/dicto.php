e64a651ce1c6e5886320a9d4df369cb6737fa6f3
Richard Klees <richard.klees@rwth-aachen.de>
2017-01-03 13:27:26 +0100

    removed unused use-statements

--- src/App/ReportCommand.php
+++ src/App/ReportCommand.php
@@ -10,11 +10,9 @@
 
 namespace Lechimp\Dicto\App;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Command to create a report.
