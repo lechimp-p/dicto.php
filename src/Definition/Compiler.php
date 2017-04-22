2861f7593cbea69ed28514a5e8f5b1a2c7f19b80
Richard Klees <richard.klees@rwth-aachen.de>
2017-04-23 00:54:18 +0200

    Compiler for ASTs.

---
+++ src/Definition/Compiler.php
@@ -0,0 +1,318 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+namespace Lechimp\Dicto\Definition;
+
+use Lechimp\Dicto\Rules\Ruleset;
+use Lechimp\Dicto\Variables as V;
+use Lechimp\Dicto\Rules as R;
+
+/**
+ * Compiles an AST to a ruleset.
+ */
+class Compiler implements ArgumentParser {
+    /**
+     * @var V\Variable[]
+     */
+    protected $predefined_variables;
+
+    /**
+     * @var R\Schema[]
+     */
+    protected $schemas;
+
+    /**
+     * @var array<string,R\Property[]>
+     */
+    protected $properties;
+
+    /**
+     * @var array<string,V\Variable[]>
+     */
+    protected $variables = array();
+
+    /**
+     * @var R\Rule[]
+     */
+    protected $rules = array();
+
+    /**
+     * @var string|null
+     */
+    protected $last_explanation = null;
+
+    /**
+     * @var Parameter[]|null
+     */
+    protected $current_parameters = [];
+
+    /**
+     * @param   V\Variable[]    $predefined_variables
+     * @param   R\Schema[]      $schemas
+     * @param   V\Property[]    $properties
+     */
+    public function __construct( array $predefined_variables
+                               , array $schemas
+                               , array $properties) {
+        $this->predefined_variables = array_map(function(V\Variable $v) {
+            return $v;
+        }, $predefined_variables);
+
+        $this->schemas = [];
+        foreach ($schemas as $schema) (function(R\Schema $s) {
+            $this->schemas[$s->name()] = $s;
+        })($schema);
+
+        $this->properties = [];
+        foreach ($properties as $property) (function(V\Property $p) {
+            $this->properties[$p->parse_as()] = $p;
+        })($property);
+    }
+
+    /**
+     * Compile an AST to an according entity.
+     *
+     * @param   AST\Node    $node
+     * @return  mixed
+     */
+    public function compile(AST\Root $node) {
+        $this->variables = array();
+        $this->rules = array();
+        $this->current_parameters = [];
+        $this->add_predefined_variables();
+        return $this->compile_root($node);
+    }
+
+    protected function compile_root(AST\Root $node) {
+        foreach ($node->lines() as $line) {
+            if ($line instanceof AST\Assignment) {
+                $this->compile_assignment($line);
+                $this->last_explanation = null;
+            }
+            else if ($line instanceof AST\Rule) {
+                $this->compile_rule($line);
+                $this->last_explanation = null;
+            }
+            else if ($line instanceof AST\Explanation) {
+                $this->last_explanation = $line;
+            }
+            else {
+                throw new \LogicException("Can't compile '".get_class($line)."'");
+            }
+        }
+        $this->purge_predefined_variables();
+        return new Ruleset($this->variables, $this->rules);
+    }
+
+    protected function compile_assignment(AST\Assignment $node) {
+        $def = $this->compile_definition($node->definition());
+        if ($this->last_explanation !== null) {
+            $def = $def->withExplanation($this->last_explanation->content());
+        }
+        $this->add_variable
+            ("".$node->name()
+            , $def
+            );
+    }
+
+    protected function compile_rule(AST\Rule $node) {
+        $schema = $this->get_schema("".$node->id());
+        $rule = new R\Rule
+            ( $this->compile_qualifier($node->qualifier())
+            , $this->compile_definition($node->left())
+            , $schema
+            , $this->compile_parameters($schema, $node->parameters())
+            );
+        if ($this->last_explanation !== null) {
+            $rule = $rule->withExplanation($this->last_explanation->content());
+        }
+        $this->rules[] = $rule;
+    }
+
+    protected function compile_definition(AST\Definition $node) {
+        if ($node instanceof AST\Name) {
+            return $this->get_variable("$node");
+        }
+        if ($node instanceof AST\Any) {
+            return $this->compile_any($node);
+        }
+        if ($node instanceof AST\Except) {
+            return $this->compile_except($node);
+        }
+        if ($node instanceof AST\Property) {
+            return $this->compile_property($node);
+        }
+        throw new \LogicException("Can't compile '".get_class($node)."'");
+    }
+
+    protected function compile_any(AST\Any $node) {
+        $definitions = $node->definitions();
+
+        // short circuit for any with only one element.
+        if (count($definitions) == 1) {
+            return $this->compile_definition($definitions[0]);
+        }
+
+        return new V\Any
+            (array_map
+                ( [$this, "compile_definition"]
+                , $definitions
+                )
+            );
+    }
+
+    protected function compile_except(AST\Except $node) {
+        return new V\Except
+            ( $this->compile_definition($node->left())
+            , $this->compile_definition($node->right())
+            );
+    }
+
+    protected function compile_property(AST\Property $node) {
+        $variable = $this->compile_definition($node->left());
+        $property = $this->get_property("".$node->id());
+        $parameters = $this->compile_parameters($property, $node->parameters());
+        return new V\WithProperty($variable, $property, $parameters);
+    }
+
+    protected function compile_parameters($prop_or_schema, array $parameters) {
+        assert('$prop_or_schema instanceof Lechimp\Dicto\Variable\Property || $prop_or_schema instanceof Lechimp\Dicto\Rules\Rule');
+        // TODO: rename fetch_arguments to fetch_parameters
+        $this->current_parameters = $parameters;
+        $parameters = $prop_or_schema->fetch_arguments($this);
+        if (count($this->current_parameters) > 0) {
+            // TODO: add line info here.
+            throw new \ParserException("Too much parameters.");
+        }
+        return $parameters;
+    }
+
+    protected function compile_qualifier(AST\Qualifier $node) {
+        $which = $node->which();
+        if ($which === AST\Qualifier::MUST) {
+            return R\Rule::MODE_MUST;
+        }
+        if ($which == AST\Qualifier::CANNOT) {
+            return R\Rule::MODE_CANNOT;
+        }
+        if ($which == AST\Qualifier::ONLY_X_CAN) {
+            return R\Rule::MODE_ONLY_CAN;
+        }
+        throw new \LogicException("Unknown qualifier '$which'");
+    }
+
+    /**
+     * Add a variable to the variables currently known.
+     *
+     * @param   string      $name
+     * @param   V\Variable  $def
+     * @return null
+     */
+    protected function add_variable($name, V\Variable $def) {
+        assert('is_string($name)');
+        if (array_key_exists($name, $this->variables)) {
+            throw new ParserException("Variable '$name' already defined.");
+        }
+        assert('$def instanceof Lechimp\\Dicto\\Variables\\Variable');
+        $this->variables[$name] = $def->withName($name);
+    }
+
+    /**
+     * Get a predefined variable.
+     *
+     * @param   string  $name
+     * @return  V\Variable
+     */
+    protected function get_variable($name) {
+        if (!array_key_exists($name, $this->variables)) {
+            throw new ParserException("Unknown variable '$name'.");
+        }
+        return $this->variables[$name];
+    }
+
+    /**
+     * Get a property. Yes. Really.
+     *
+     * @param   string  $name
+     * @return  V\Property
+     */
+    protected function get_property($name) {
+        if (!array_key_exists($name, $this->properties)) {
+            throw new ParserException("Unknown property '$name'.");
+        }
+        return $this->properties[$name];
+    }
+
+    /**
+     * Get a schema. By name.
+     *
+     * @param   string  $name
+     * @return  V\Schema
+     */
+    protected function get_schema($name) {
+        if (!array_key_exists($name, $this->schemas)) {
+            throw new ParserException("Unknown schema '$name'.");
+        }
+        return $this->schemas[$name];
+    }
+
+    /**
+     * Add all predefined variables to the current set of variables.
+     *
+     * @return null
+     */
+    protected function add_predefined_variables() {
+        foreach ($this->predefined_variables as $predefined_var) {
+            $this->add_variable($predefined_var->name(), $predefined_var);
+        }
+    }
+
+    /**
+     * Purge all predefined variables from the current set of variables.
+     *
+     * @return null
+     */
+    protected function purge_predefined_variables() {
+        foreach ($this->predefined_variables as $predefined_var) {
+            unset($this->variables[$predefined_var->name()]);
+        }
+    }
+
+    // IMPLEMENTATION OF ArgumentParser
+
+    /**
+     * @inheritdoc
+     */
+    public function fetch_string() {
+        $node = $this->next_current_parameter(AST\StringValue::class);
+        return "".$node;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function fetch_variable() {
+        $node = $this->next_current_parameter(AST\Definition::class);
+        return $this->compile_definition($node);
+    }
+
+    protected function next_current_parameter($class) {
+        if (count($this->current_parameters) == 0) {
+            // TODO: add location info
+            throw new \ParserException("Tried to fetch an argument, but none was left.");
+        }
+        $arg = array_shift($this->current_parameters);
+        if (!($arg instanceof $class)) {
+            // TODO: add location info
+            throw new \ParserException("Tried to fetch a '%class' argument but next is '".get_class($arg)."'");
+        }
+        return $arg;
+    }
+}
---
+++ tests/CompilerTest.php
@@ -0,0 +1,556 @@
+<?php
+/******************************************************************************
+ * An implementation of dicto (scg.unibe.ch/dicto) in and for PHP.
+ *
+ * Copyright (c) 2016, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received
+ * a copy of the license along with the code.
+ */
+
+use Lechimp\Dicto\Regexp;
+use Lechimp\Dicto\Definition\AST;
+use Lechimp\Dicto\Definition\Compiler;
+use Lechimp\Dicto\Rules\Ruleset;
+use Lechimp\Dicto\Variables as V;
+use Lechimp\Dicto\Rules as R;
+
+class _Compiler extends Compiler {
+    public function _compile_definition($node) {
+        return $this->compile_definition($node);
+    }
+    public function _add_predefined_variables() {
+        $this->add_predefined_variables();
+    }
+}
+
+class CompilerTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        $this->f = new AST\Factory();
+        $this->compiler = new _Compiler
+            ( array
+                ( new V\Namespaces()
+                , new V\Classes()
+                , new V\Interfaces()
+                , new V\Traits()
+                , new V\Functions()
+                , new V\Globals()
+                , new V\Files()
+                , new V\Methods()
+                , new V\ErrorSuppressor()
+                , new V\ExitOrDie()
+                )
+            , array
+                ( new R\ContainText()
+                )
+            , array
+                ( new V\Name()
+                , new V\In()
+                )
+            );
+    }
+
+    public function compile($ast) {
+        return $this->compiler->compile($ast);
+    }
+
+    public function test_empty() {
+        $res = $this->compile($this->f->root([]));
+        $this->assertEquals(new Ruleset(array(), array()), $res);
+    }
+
+    public function test_variable() {
+        $res = $this->compile
+            ( $this->f->root
+                ([ $this->f->assignment
+                    ( $this->f->name("AllClasses")
+                    , $this->f->name("Classes")
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "AllClasses" => new V\Classes("AllClasses")
+            );
+
+        $this->assertEquals($expected, $res->variables());
+    }
+
+    public function test_error_suppressor() {
+        $res = $this->compile
+            ( $this->f->root
+                ([ $this->f->assignment
+                    ( $this->f->name("TheErrorSuppressor")
+                    , $this->f->name("ErrorSuppressor")
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "TheErrorSuppressor" => new V\ErrorSuppressor("TheErrorSuppressor")
+            );
+
+        $this->assertEquals($expected, $res->variables());
+    }
+
+    public function test_interface_variable() {
+        $res = $this->compile
+            ( $this->f->root
+                ([ $this->f->assignment
+                    ( $this->f->name("AllInterfaces")
+                    , $this->f->name("Interfaces")
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "AllInterfaces" => new V\Interfaces("AllInterfaces")
+            );
+
+        $this->assertEquals($expected, $res->variables());
+    }
+
+    public function test_trait_variable() {
+        $res = $this->compile
+            ( $this->f->root
+                ([ $this->f->assignment
+                    ( $this->f->name("AllTraits")
+                    , $this->f->name("Traits")
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "AllTraits" => new V\Traits("AllTraits")
+            );
+
+        $this->assertEquals($expected, $res->variables());
+    }
+
+    public function test_namespace_variable() {
+        $res = $this->compile
+            ( $this->f->root
+                ([ $this->f->assignment
+                    ( $this->f->name("AllNamespaces")
+                    , $this->f->name("Namespaces")
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "AllNamespaces" => new V\Namespaces("AllNamespaces")
+            );
+
+        $this->assertEquals($expected, $res->variables());
+    }
+
+    public function test_classes_in_namespaces_with_name_1() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->property
+                ( $this->f->name("Classes")
+                , $this->f->atom("in")
+                , [ $this->f->property
+                    ( $this->f->name("Namespaces")
+                    , $this->f->atom("with name")
+                    , [ $this->f->string_value("foo.*") ]
+                    )]
+                )
+            );
+
+        $expected = new V\WithProperty
+            ( new V\Classes
+            , new V\In
+            , array
+                ( new V\WithProperty
+                    ( new V\Namespaces
+                    , new V\Name
+                    , array(new Regexp("foo.*"))
+                    )
+                )
+            );
+
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_classes_in_namespaces_with_name_2() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->property
+                ( $this->f->property
+                    ( $this->f->name("Classes")
+                    , $this->f->atom("in")
+                    , [ $this->f->name("Namespaces") ]
+                    )
+                , $this->f->atom("with name")
+                , [ $this->f->string_value("foo.*") ]
+                )
+            );
+
+        $expected = new V\WithProperty
+            ( new V\WithProperty
+                ( new V\Classes
+                , new V\In
+                , array(new V\Namespaces)
+                )
+            , new V\Name
+            , array(new Regexp("foo.*"))
+            );
+
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_variables() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->assignment
+                    ( $this->f->name("AllClasses")
+                    , $this->f->name("Classes")
+                    )
+                , $this->f->assignment
+                    ( $this->f->name("AllFunctions")
+                    , $this->f->name("Functions")
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "AllClasses" => new V\Classes("AllClasses")
+            , "AllFunctions" => new V\Functions("AllFunctions")
+            );
+
+        $this->assertEquals($expected, $res->variables());
+    }
+
+    public function test_any_short_circuit() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->any
+                ( [$this->f->name("Classes")] )
+            );
+
+        $expected = new V\Classes();
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_any() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->any
+                ([$this->f->name("Classes")
+                , $this->f->name("Functions")
+                ])
+            );
+
+        $expected = new V\Any(array
+            ( new V\Classes()
+            , new V\Functions()
+            ));
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_except() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->except
+                ( $this->f->name("Classes")
+                , $this->f->name("Functions")
+                )
+            );
+
+        $expected = new V\Except
+            ( new V\Classes()
+            , new V\Functions()
+            );
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_any_except() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->except
+                ( $this->f->any
+                    ([$this->f->except
+                        ( $this->f->name("Classes")
+                        , $this->f->name("Functions")
+                        )
+                    , $this->f->name("Methods")
+                    ])
+                , $this->f->name("Globals")
+                )
+            );
+
+        $expected = new V\Except
+            ( new V\Any(array
+                ( new V\Except
+                    ( new V\Classes()
+                    , new V\Functions()
+                    )
+                , new V\Methods()
+                ))
+            , new V\Globals()
+            );
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_with_name() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->property
+                ( $this->f->name("Classes")
+                , $this->f->atom("with name")
+                , [$this->f->string_value("foo")]
+                )
+            );
+
+        $expected = new V\WithProperty
+            ( new V\Classes()
+            , new V\Name
+            , array(new Regexp("foo"))
+            );
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_with_name_assignment() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->assignment
+                    ( $this->f->name("Foo")
+                    , $this->f->property
+                        ( $this->f->name("Classes")
+                        , $this->f->atom("with name")
+                        , [$this->f->string_value("foo")]
+                        )
+                    )
+                ])
+            );
+
+        $expected = array
+            ( "Foo" => (new V\WithProperty
+                ( new V\Classes()
+                , new V\Name
+                , array(new Regexp("foo"))
+                ))
+                ->withName("Foo")
+            );
+        $this->assertEquals($expected, $res->variables());
+
+    }
+
+    public function test_methods_in_classes() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->property
+                ( $this->f->name("Methods")
+                , $this->f->atom("in")
+                , [$this->f->name("Classes")]
+                )
+            );
+
+        $expected = new V\WithProperty
+            ( new V\Methods()
+            , new V\In
+            , array(new V\Classes())
+            );
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_exit_or_die() {
+        $this->compiler->_add_predefined_variables();
+        $res = $this->compiler->_compile_definition
+            ( $this->f->name("ExitOrDie")
+            );
+
+        $expected = new V\ExitOrDie();
+        $this->assertEquals($expected, $res);
+    }
+
+    public function test_classes_cannot_contain_text() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->rule
+                    ( $this->f->name("Classes")
+                    , $this->f->cannot()
+                    , $this->f->atom("contain text")
+                    , [$this->f->string_value("foo")]
+                    )
+                ])
+            );
+
+        $expected = array
+            ( new R\Rule
+                ( R\Rule::MODE_CANNOT
+                , new V\Classes()
+                , new R\ContainText
+                , array(new Regexp("foo"))
+                )
+            );
+        $this->assertEquals($expected, $res->rules());
+    }
+
+    public function test_classes_must_contain_text() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->rule
+                    ( $this->f->name("Classes")
+                    , $this->f->must()
+                    , $this->f->atom("contain text")
+                    , [$this->f->string_value("foo")]
+                    )
+                ])
+            );
+
+        $expected = array
+            ( new R\Rule
+                ( R\Rule::MODE_MUST
+                , new V\Classes()
+                , new R\ContainText
+                , array(new Regexp("foo"))
+                )
+            );
+        $this->assertEquals($expected, $res->rules());
+    }
+
+    public function test_only_classes_can_contain_text() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->rule
+                    ( $this->f->name("Classes")
+                    , $this->f->only_X_can()
+                    , $this->f->atom("contain text")
+                    , [$this->f->string_value("foo")]
+                    )
+                ])
+            );
+
+        $expected = array
+            ( new R\Rule
+                ( R\Rule::MODE_ONLY_CAN
+                , new V\Classes()
+                , new R\ContainText
+                , array(new Regexp("foo"))
+                )
+            );
+        $this->assertEquals($expected, $res->rules());
+    }
+
+    public function test_only_classes_and_methods_can_contain_text() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->rule
+                    ( $this->f->any
+                        ([$this->f->name("Classes")
+                        , $this->f->name("Methods")
+                        ])
+                    , $this->f->only_X_can()
+                    , $this->f->atom("contain text")
+                    , [$this->f->string_value("foo")]
+                    )
+                ])
+            );
+
+        $expected = array
+            ( new R\Rule
+                ( R\Rule::MODE_ONLY_CAN
+                , new V\Any(array
+                    ( new V\Classes()
+                    , new V\Methods()
+                    ))
+                , new R\ContainText
+                , array(new Regexp("foo"))
+                )
+            );
+        $this->assertEquals($expected, $res->rules());
+    }
+
+    public function test_only_classes_and_methods_in_classes_can_contain_text() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->rule
+                    ( $this->f->any
+                        ([$this->f->name("Classes")
+                        , $this->f->property
+                            ( $this->f->name("Methods")
+                            , $this->f->atom("in")
+                            , [$this->f->name("Classes")]
+                            )
+                        ])
+                    , $this->f->only_X_can()
+                    , $this->f->atom("contain text")
+                    , [$this->f->string_value("foo")]
+                    )
+                ])
+            );
+
+        $expected = array
+            ( new R\Rule
+                ( R\Rule::MODE_ONLY_CAN
+                , new V\Any(array
+                    ( new V\Classes()
+                    , new V\WithProperty
+                        ( new V\Methods()
+                        , new V\In()
+                        , array(new V\Classes())
+                        )
+                    ))
+                , new R\ContainText
+                , array(new Regexp("foo"))
+                )
+            );
+        $this->assertEquals($expected, $res->rules());
+    }
+
+    public function test_explain_variables() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->explanation("Explanation")
+                , $this->f->assignment
+                    ( $this->f->name("AllClasses")
+                    , $this->f->name("Classes")
+                    )
+                ])
+            );
+
+        $res = $res->variables()["AllClasses"];
+        $this->assertInstanceOf(V\Classes::class, $res);
+        $this->assertEquals("Explanation", $res->explanation());
+    }
+
+    public function test_explain_rules() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->explanation("Explanation")
+                , $this->f->rule
+                    ( $this->f->name("Classes")
+                    , $this->f->cannot()
+                    , $this->f->atom("contain text")
+                    , [$this->f->string_value("name")]
+                    )
+                ])
+            );
+
+        $res = $res->rules()[0];
+        $this->assertInstanceOf(R\Rule::class, $res);
+        $this->assertEquals("Explanation", $res->explanation());
+    }
+
+    public function test_no_double_explanation() {
+        $res = $this->compile
+            ( $this->f->root
+                ([$this->f->explanation("Explanation")
+                , $this->f->assignment
+                    ( $this->f->name("AllClasses")
+                    , $this->f->name("Classes")
+                    )
+                , $this->f->assignment
+                    ( $this->f->name("AllFunctions")
+                    , $this->f->name("Functions")
+                    )
+                ])
+            );
+
+        $res = $res->variables()["AllFunctions"];
+        $this->assertInstanceOf(V\Functions::class, $res);
+        $this->assertSame(null, $res->explanation());
+    }
+}
